06f1a941bf8ae501314d3e00b5708a7e39d5431b
MJSoto123 <68660162+MJSoto123@users.noreply.github.com>
2022-08-19 23:16:39 -0500

    Create inscription.service.js

---
+++ src/aplication/services/inscription.service.js
@@ -0,0 +1,13 @@
+const BaseRepository = require("./base.repository");
+
+class InscriptionRepository extends BaseRepository {
+  constructor(InscriptionDb) {
+    super(InscriptionDb);
+  }
+
+  async getAllWithoutPagination() {
+    return await this.model.find();
+  }
+}
+
+module.exports = InscriptionRepository
